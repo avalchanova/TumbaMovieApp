1916b53f0e5abe01f50044db095abf99170604eb
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-11-30 19:27:34 +0200

    cleanup

--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Navigation.module.css';
+
 const Navigation = () => {
     return (
         <nav className={styles.navigation}>
