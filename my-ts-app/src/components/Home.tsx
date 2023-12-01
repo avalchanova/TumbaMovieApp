15ac6f4dda646810c99518a93d7e70aefd5eafd8
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-12-01 17:14:33 +0200

    image.tsx, home.tsx

---
+++ my-ts-app/src/components/Home.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import MovieList from './MovieList'
+import { useFetchMovies } from '../hooks/useFetchMovies'
+
+const Home = () => {
+  const { movies, loading, error } = useFetchMovies()
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    console.log(error)
+    return <div>No movies in DB</div>
+  }
+
+  return (
+    <div className='home'>
+      <h1>Movie List</h1>
+      {movies && <MovieList movies={movies} />}
+    </div>
+  )
+}
+
+export default Home
---
+++ my-ts-app/src/components/Image.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Movie } from '../store/feature/movieSlice'
+import noImage from '../assets/noImage.jpeg'
+
+interface ImageProps {
+  movie: Movie
+}
+
+export const Image = ({ movie }: ImageProps) => {
+  if (!movie.primaryImage) {
+    return <img src={noImage} alt={'Not available'} />
+  }
+
+  return <img src={movie.primaryImage.url} alt={movie.titleText.text} />
+}
