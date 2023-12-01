6847e4b14862e19ccbff659864fd576076d4c9cb
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-12-01 17:38:55 +0200

    renaming movies to moviesState

--- my-ts-app/src/store/store.ts
+++ my-ts-app/src/store/store.ts
@@ -4,7 +4,7 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 export const store = configureStore({
   reducer: {
-    movies: MovieSlice.reducer,
+    moviesState: MovieSlice.reducer,
   },
 })
 
