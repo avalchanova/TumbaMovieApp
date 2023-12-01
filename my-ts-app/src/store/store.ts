b8e35111ff601977cdb45f8f594ab7469ab75898
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-12-01 17:12:34 +0200

    dispatch and selector

---
+++ my-ts-app/src/store/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { MovieSlice } from './feature/movieSlice'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+
+export const store = configureStore({
+  reducer: {
+    movies: MovieSlice.reducer,
+  },
+})
+
+export const useAppDispatch: () => typeof store.dispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<
+  ReturnType<typeof store.getState>
+> = useSelector
+
+// вкарваме данни с Dispatch
+// изкарваме данни със Selector
+// store.ts is a little bit like App.tsx
