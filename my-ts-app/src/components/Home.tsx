dcfe99da35c9ee47b1fab841a487677faf504e54
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-12-04 19:23:39 +0100

    search box + functionality

--- my-ts-app/src/components/Home.tsx
+++ my-ts-app/src/components/Home.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MovieList from './MovieList'
 import { useFetchMovies } from '../hooks/useFetchMovies'
+import { Movie } from '../store/feature/movieSlice'
 
 const Home = () => {
     const { movies, loading, error } = useFetchMovies()
+    const [searchQuery, setSearchQuery] = useState('')
+    const [filteredMovies, setFilteredMovies] = useState<Movie[]>([])
+    // <Movie[]> provides type information for the array elements,
+    // and([]) initializes the state with an empty array of Movie objects
 
     if (loading) {
         return <div>Loading...</div>
@@ -14,10 +19,39 @@ const Home = () => {
         return <div>No movies in DB</div>
     }
 
+    if (!movies) {
+        return null
+    }
+
+    const handleSearch = () => {
+        const filtered = movies.filter((movie) =>
+            movie.titleText.text
+                .toLowerCase()
+                .includes(searchQuery.toLowerCase())
+        )
+        setFilteredMovies(filtered)
+    }
+
     return (
         <div className='home'>
             <h1>Movie List</h1>
-            {movies && <MovieList movies={movies} />}
+            <input
+                type='search'
+                id='movie-search'
+                name='movie-search'
+                placeholder='Search for a movie'
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button className='' onClick={handleSearch}>
+                🔍
+            </button>
+
+            {searchQuery ? (
+                <MovieList movies={filteredMovies} />
+            ) : (
+                movies && <MovieList movies={movies} />
+            )}
         </div>
     )
 }
